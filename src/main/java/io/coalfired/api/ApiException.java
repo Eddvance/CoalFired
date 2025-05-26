3db791b9e4f4d5677097a70b22ecbcb1f1b68048
Edouard Leroy <leroyedouard@protonmail.com>
2025-05-26 18:35:33 +0200

    First commit

--- src/main/java/io/coalfired/api/ApiException.java
+++ src/main/java/io/coalfired/api/ApiException.java
@@ -3,7 +3,8 @@ package io.coalfired.api;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2025-05-22T16:36:15.642271455Z[GMT]")
 public class ApiException extends Exception {
     private int code;
-    public ApiException (int code, String msg) {
+
+    public ApiException(int code, String msg) {
         super(msg);
         this.code = code;
     }
--- src/main/java/io/coalfired/api/ApiOriginFilter.java
+++ src/main/java/io/coalfired/api/ApiOriginFilter.java
@@ -1,17 +1,17 @@
 package io.coalfired.api;
 
-import java.io.IOException;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2025-05-22T16:36:15.642271455Z[GMT]")
 public class ApiOriginFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
+                         FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.addHeader("Access-Control-Allow-Origin", "http://localhost:8081");// CORS IGNORE
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         res.addHeader("Access-Control-Allow-Headers", "Content-Type");
         chain.doFilter(request, response);
